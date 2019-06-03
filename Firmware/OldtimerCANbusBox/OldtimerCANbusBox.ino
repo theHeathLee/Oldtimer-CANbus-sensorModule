f8a445163d78609209c5967ebcd9613ede16e820
heath <heath.lee.ee@gmail.com>
2019-06-03 22:35:26 +0200

    added basic can functionality

--- Firmware/OldtimerCANbusBox/OldtimerCANbusBox.ino
+++ Firmware/OldtimerCANbusBox/OldtimerCANbusBox.ino
@@ -1,9 +1,32 @@
+#include <mcp_can.h>
+#include <mcp_can_dfs.h>
+#include <SPI.h>
+
+MCP_CAN CAN0(10);     // Set CS to pin 10
+byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(115200);
+  
+// Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
+  else Serial.println("Error Initializing MCP2515...");
+
+  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
+//send can messages
+// send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+  if(sndStat == CAN_OK){
+    Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(100);   // send data per 100ms
 }
